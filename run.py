ddd0d02b995d9e38323b2a297753775d77678fc7
Mingjie Shen <shen497@purdue.edu>
2022-11-08 22:24:59 -0500

    move firmware list to a separate file

--- run.py
+++ run.py
@@ -1,18 +1,21 @@
-from subprocess import check_output
+import subprocess
+import re
 
-firmware_list = [
-("Infinitime", "/home/smj/purs3/InfiniTime/build/src/pinetime-app-1.10.0.out.bc"),
-("nrf52_kbd_rev_c", "/home/smj/purs3/nrf52-keyboard/keyboard/lot60-ble/_build/rev_c/nrf52_kbd.out.bc"),
-("nrf52_kbd_rev_e", "/home/smj/purs3/nrf52-keyboard/keyboard/lot60-ble/_build/rev_e/nrf52_kbd.out.bc"),
-("nrf52_kbd_rev_e_3led", "/home/smj/purs3/nrf52-keyboard/keyboard/lot60-ble/_build/rev_e_3led/nrf52_kbd.out.bc"),
-("nrf52_kbd_rev_f", "/home/smj/purs3/nrf52-keyboard/keyboard/lot60-ble/_build/rev_f/nrf52_kbd.out.bc"),
-("nrf52_kbd_rev_f_3", "/home/smj/purs3/nrf52-keyboard/keyboard/lot60-ble/_build/rev_f_3led/nrf52_kbd.out.bc"),
-("nrf52_kbd_rev_g", "/home/smj/purs3/nrf52-keyboard/keyboard/lot60-ble/_build/rev_g/nrf52_kbd.out.bc"),
-("nrf52_kbd_rev_g_3led", "/home/smj/purs3/nrf52-keyboard/keyboard/lot60-ble/_build/rev_g_3led/nrf52_kbd.out.bc"),
-("nrf52_kbd_rev_g_832", "/home/smj/purs3/nrf52-keyboard/keyboard/lot60-ble/_build/rev_g_832/nrf52_kbd.out.bc"),
-]
+firmware_list = []
 
 LLVM_DIR='/usr/lib/llvm-14'
 
+with open("firmware.txt") as f:
+    for line in f:
+        bc = line.strip()
+        if bc:
+            name = re.sub(r"\.[^.]+\.(bc|ll)$", "", bc)  # remove ".xxx.(bc|ll)"
+            name = re.sub(r"(purs3|_?build)", "", name)
+            name = re.sub(r"[^\w\s-]", "-", name)
+            name = re.sub(r"[-\s]+", "-", name).strip("-_")
+            firmware_list.append((name, bc))
+
+subprocess.check_output("mkdir -p results", shell=True)
+
 for name, bc in firmware_list:
-    check_output(f"{LLVM_DIR}/bin/opt -load-pass-plugin lib/libFindMMIOFunc.so -load-pass-plugin lib/libFindHALBypass.so --passes='print<hal-bypass>' --disable-output {bc} 2> {name}.out", shell=True)
+    subprocess.run(f"{LLVM_DIR}/bin/opt -load-pass-plugin build/lib/libFindMMIOFunc.so -load-pass-plugin build/lib/libFindHALBypass.so --passes='print<hal-bypass>' --disable-output {bc} 2> results/{name}.out", shell=True)
