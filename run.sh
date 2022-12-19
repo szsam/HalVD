931068f06d8c54329814da0e48070b638ae78a2f
Mingjie Shen <shen497@purdue.edu>
2022-12-19 15:12:30 +0000

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+LLVM_DIR="/usr/lib/llvm-14"
+
+BITCODES=$(find "$RTOSExploration/bitcode-db/zephyr-samples/96b_stm32_sensor_mez" -name "*.bc")
+
+parallel -i sh -c "${LLVM_DIR}/bin/opt \
+  -load-pass-plugin build/lib/libFindMMIOFunc.so \
+  -load-pass-plugin build/lib/libFindHALBypass.so \
+  --passes='print<hal-bypass>' --disable-output {} 2> {}.analysis" -- $BITCODES
+
