6b1cd78370d9390eb50505150950c867d1118fbe
Mingjie Shen <shen497@purdue.edu>
2023-01-04 15:40:59 +0000

    use *.ll for esp-idf-examples

--- run.sh
+++ run.sh
@@ -3,7 +3,10 @@
 LLVM_DIR="/usr/lib/llvm-14"
 
 #BITCODES=$(find "$RTOSExploration/bitcode-db/Amazfitbip" -name "*.bc")
-BITCODES=$(find "$RTOSExploration/bitcode-db/" -name "*.bc")
+BITCODES=$(find "$RTOSExploration/bitcode-db/" \
+  -not \( -path "$RTOSExploration/bitcode-db/esp-idf-examples" -prune \) -name "*.bc")
+BITCODES_ESP_IDF=$(find "$RTOSExploration/bitcode-db/esp-idf-examples" -name "*.ll")
+BITCODES="$BITCODES $BITCODES_ESP_IDF"
 
 parallel -i sh -c "${LLVM_DIR}/bin/opt \
   -load-pass-plugin build/lib/libFindMMIOFunc.so \
