d39a0a8deeada6c638d1de0ee0c234c76b1ab279
Mingjie Shen <shen497@purdue.edu>
2023-01-15 06:08:17 +0000

    add a summary of analyses

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 LLVM_DIR="/usr/lib/llvm-14"
 
-#BITCODES=$(find "$RTOSExploration/bitcode-db/Amazfitbip" -name "*.bc")
+#BITCODES=$(find "$RTOSExploration/bitcode-db/zephyr-samples" -name "*.bc")
 BITCODES=$(find "$RTOSExploration/bitcode-db/" \
   -not \( -path "$RTOSExploration/bitcode-db/esp-idf-examples" -prune \) -name "*.bc")
 BITCODES_ESP_IDF=$(find "$RTOSExploration/bitcode-db/esp-idf-examples" -name "*.ll")
@@ -13,3 +13,6 @@ parallel -i sh -c "${LLVM_DIR}/bin/opt \
   -load-pass-plugin build/lib/libFindHALBypass.so \
   --passes='print<hal-bypass>' --disable-output {} 2> {}.analysis" -- $BITCODES
 
+find "$RTOSExploration/bitcode-db/" -name "*.analysis" \
+  -exec bash -c "echo -ne '{} '; tail -n 1 '{}'; echo" \; \
+  > "$RTOSExploration/bitcode-db/summary.txt"
